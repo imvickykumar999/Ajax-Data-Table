eb4641503fbbfea0022444c56b19abd50fb8545e
Vicky Kumar <imvickykumar999@gmail.com>
2024-12-11 21:24:31 +0530

    added css and filter

--- scroll_project/scroll_project/table_app/views.py
+++ scroll_project/scroll_project/table_app/views.py
@@ -5,8 +5,13 @@ from django.shortcuts import render
 
 def load_rows(request):
     page = request.GET.get('page', 1)
+    filter_value = request.GET.get('filter', '')
     rows_per_page = 20
+
     rows = TableRow.objects.all().order_by('id')
+    if filter_value:
+        rows = rows.filter(name=filter_value)  # Adjust the field name accordingly
+
     paginator = Paginator(rows, rows_per_page)
     page_obj = paginator.get_page(page)
     data = [
@@ -16,4 +21,8 @@ def load_rows(request):
     return JsonResponse({"rows": data, "has_next": page_obj.has_next()})
 
 def index(request):
-    return render(request, 'table_app/index.html')
+    range100 = range(1, 101)
+    context = {
+        'range100' : range100,
+    }
+    return render(request, 'table_app/index.html', context=context)
